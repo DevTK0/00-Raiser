002a0665a25d49ca1b102e311e68d03e441661d0
devtk0 <tko.developer.02@gmail.com>
2023-01-01 15:45:34 +0800

    Added error handling to load and unload commands.

--- discord/app/bot.py
+++ discord/app/bot.py
@@ -2,6 +2,7 @@ import discord
 import os
 from app.settings import DISCORD_AUTH_TOKEN
 from discord.ext import commands
+from app.handlers import output
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -10,13 +11,27 @@ bot = commands.Bot(command_prefix=("00R "), case_insensitive=True, intents=inten
 
 @bot.command()
 async def load(ctx, extension):
-    await bot.load_extension(f'app.cogs.{extension}')
-    await ctx.send(f'Loaded {extension}')
+
+    embed = output.embed(title="Load Extension", description = f'Loading {extension}')
+    message = await ctx.send(embed=embed)
+
+    try:
+        await bot.load_extension(f'app.cogs.{extension}')
+        await message.edit(embed=output.update(embed, description = f'Loaded {extension}'))
+    except Exception as e:
+        await message.edit(embed=output.error(embed, e))
 
 @bot.command()
 async def unload(ctx, extension):
-    await bot.unload_extension(f'app.cogs.{extension}')
-    await ctx.send(f'Unloaded {extension}')
+
+    embed = output.embed(title="Unload Extension", description = f'Unloading {extension}')
+    message = await ctx.send(embed=embed)
+
+    try:
+        await bot.unload_extension(f'app.cogs.{extension}')
+        await message.edit(embed=output.update(embed, description = f'Unloaded {extension}'))
+    except Exception as e:
+        await message.edit(embed=output.error(embed, e))
 
 @bot.event
 async def on_ready():
--- discord/app/handlers/output.py
+++ discord/app/handlers/output.py
@@ -10,19 +10,24 @@ VRISING_THUMBNAIL="https://cdn.discordapp.com/icons/803241158054510612/a_7dcd3bc
 MINECRAFT_THUMBNAIL="https://cdn.discordapp.com/icons/302094807046684672/a_4a2d4c71d0ec0c7f72792d7280a6529d.webp?size=32"
 COREKEEPER_THUMBNAIL="https://cdn.discordapp.com/icons/851842678340845600/1288f168ce7d27e283fd922569e458d0.webp?size=32"
 
-def embed(title, description, thumbnail):
-    embed = discord.Embed(title=title, description=description, color=LOADING) 
+def embed(title="", description="", thumbnail=None, color=LOADING):
+    embed = discord.Embed(title=title, description=description, color=color) 
     embed.set_thumbnail(url=thumbnail)
     return embed
 
+def update(embed, description="", color=DEFAULT):
+    embed.description=description
+    embed.color=color
+    return embed
+
 def vrising_status():
-    return embed("V Rising", "Getting server status.", VRISING_THUMBNAIL)
+    return embed("V Rising", "Getting server status.", VRISING_THUMBNAIL, LOADING)
 
 def vrising_start():
-    return embed("V Rising", "Server is starting.", VRISING_THUMBNAIL)
+    return embed("V Rising", "Server is starting.", VRISING_THUMBNAIL, LOADING)
 
 def vrising_stop():
-    return embed("V Rising", "Server is stopping.", VRISING_THUMBNAIL)
+    return embed("V Rising", "Server is stopping.", VRISING_THUMBNAIL, LOADING)
 
 def server_running(embed, ip):
     embed.description="Server is running."
@@ -51,7 +56,7 @@ def set_status_color(embed, status):
 
     return embed
 
-def error(embed, error, traceback):
+def error(embed, error, traceback=None):
     embed.description="Encounted an error."
     embed.color=ERROR
     embed.add_field(name="Cause", value=error, inline=False)
