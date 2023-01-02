afc973f1835d45527f6472e0984ea8f49acb7c08
devtk0 <tko.developer.02@gmail.com>
2023-01-02 16:46:52 +0800

    Changed to core keeper in tests for lower costs.

--- discord/test/aws_test.py
+++ discord/test/aws_test.py
@@ -1,44 +1,44 @@
 import pytest
 import asyncio
 from app.handlers.aws import AWS
-from app.settings import Game, Configs
+from app.settings import Game, Configs, Instance
 
 @pytest.mark.skip(reason="Only run this test in insolation, do not run with other tests.")
 def test_aws_start_server():
     with AWS() as aws:
-        response = aws.start_server(Game.V_RISING.value, Configs[Game.V_RISING])
+        response = aws.start_server(Game.CORE_KEEPER.value, Configs[Game.CORE_KEEPER])
         print(response)
         assert len(response) == 1
 
 @pytest.mark.skip(reason="Only run this test in insolation, do not run with other tests.")
 def test_aws_stop_server():
     with AWS() as aws:
-        response = aws.stop_server(Game.V_RISING.value)
+        response = aws.stop_server(Game.CORE_KEEPER.value)
         print(response)
         assert len(response["TerminatingInstances"]) == 1
 
 @pytest.mark.skip(reason="Only run this test in insolation, do not run with other tests.")
 def test_aws_wait_for_server_ip():
     with AWS() as aws:
-        response = aws.start_server(Game.V_RISING.value, Configs[Game.V_RISING])
+        response = aws.start_server(Game.CORE_KEEPER.value, Configs[Game.CORE_KEEPER])
         print(response)
-        aws.wait_for_server_ip(Game.V_RISING.value)
+        aws.wait_for_server_ip(Game.CORE_KEEPER.value)
         
-        server = aws.get_server_status(Game.V_RISING.value)
+        server = aws.get_server_status(Game.CORE_KEEPER.value)
         print(server)
         assert server["ip_address"] is not None
 
 @pytest.mark.skip(reason="Only run this test in insolation, do not run with other tests.")
 def test_aws_wait_for_server_to_stop():
     with AWS() as aws:
-        response = aws.start_server(Game.V_RISING.value, Configs[Game.V_RISING])
+        response = aws.start_server(Game.CORE_KEEPER.value, Configs[Game.CORE_KEEPER])
 
-        aws.wait_for_server_ip(Game.V_RISING.value)
+        aws.wait_for_server_ip(Game.CORE_KEEPER.value)
 
-        response = aws.stop_server(Game.V_RISING.value)
+        response = aws.stop_server(Game.CORE_KEEPER.value)
 
-        aws.wait_for_server_to_stop(Game.V_RISING.value)
+        aws.wait_for_server_to_stop(Game.CORE_KEEPER.value)
         
-        server = aws.get_server_status(Game.V_RISING.value)
+        server = aws.get_server_status(Game.CORE_KEEPER.value)
         print(server)
         assert server["status"] == "stopped"
