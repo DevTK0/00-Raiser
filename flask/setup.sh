37313412099f52e059ced95bfa42e09a2671711f
devtk0 <tko.developer.02@gmail.com>
2023-01-02 21:44:34 +0800

    Removed test project for python backend.

--- flask/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python -m venv venv
-. venv/bin/activate
-
-pip3 install -r requirements.txt
--- flask/start_flask.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gunicorn --certfile ./cert/cert.pem --keyfile ./cert/key.pem -b 0.0.0.0:8000 core:app
