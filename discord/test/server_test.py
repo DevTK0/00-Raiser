2aef155e2e0d42e695ee51deea6ff7d13c489452
devtk0 <tko.developer.02@gmail.com>
2023-01-02 16:46:40 +0800

    Changed to core keeper in tests for lower costs.

--- discord/test/server_test.py
+++ discord/test/server_test.py
@@ -6,29 +6,29 @@ from app.settings import Game, Configs
 
 def test_vrising():
     # test start server
-    response = server.start_handler(Game.V_RISING.value, Configs[Game.V_RISING])
+    response = server.start_handler(Game.CORE_KEEPER.value, Configs[Game.CORE_KEEPER])
     assert response == "Server is starting."
 
     # test starting a running server
-    response = server.start_handler(Game.V_RISING.value, Configs[Game.V_RISING])
+    response = server.start_handler(Game.CORE_KEEPER.value, Configs[Game.CORE_KEEPER])
     assert "Server is already running."
 
     with AWS() as aws:
-        aws.wait_for_server_ip(Game.V_RISING.value)
+        aws.wait_for_server_ip(Game.CORE_KEEPER.value)
 
     # test stop server
-    response = server.stop_handler(Game.V_RISING.value)
+    response = server.stop_handler(Game.CORE_KEEPER.value)
     assert response == "Server is shutting down."
 
     # test starting a stopping server
-    response = server.start_handler(Game.V_RISING.value, Configs[Game.V_RISING])
+    response = server.start_handler(Game.CORE_KEEPER.value, Configs[Game.CORE_KEEPER])
     assert response == "Server is shutting down. Please wait a few minutes and try again." 
 
     with AWS() as aws:
-        aws.wait_for_server_to_stop(Game.V_RISING.value)
+        aws.wait_for_server_to_stop(Game.CORE_KEEPER.value)
 
     # test stopping a stopped server
-    response = server.stop_handler(Game.V_RISING.value)
+    response = server.stop_handler(Game.CORE_KEEPER.value)
     assert response == "Server is already stopped." 
 
 
