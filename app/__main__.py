cd51362356762aa1f48a58766b62f86fc87286d0
DevTK0 <terence@Terences-MacBook-Air.local>
2023-12-22 08:53:50 +0800

    Added sync command to main bot instance so that it doesn't need to rely on Cogs to sync it's own slash commands.

--- app/__main__.py
+++ app/__main__.py
@@ -3,51 +3,66 @@ import os
 import traceback
 from app.settings import DISCORD_AUTH_TOKEN
 from discord.ext import commands, tasks
-from app.handlers import output
+from app.handlers import output_formatter
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix=("00R "), case_insensitive=True, intents=intents)
 
+# Only needed once per guild (used to register commands to the guild through Discord API)
+@bot.command(name="sync", description="Syncs slash commands to the guild", aliases=[])
+async def sync(ctx, guild_id):
+    
+    embed = output_formatter.OOR_sync()
+    message = await ctx.send(embed=embed) # this is a text command so we use ctx.send instead
+
+    try:
+        await bot.sync_commands(
+            guild_ids=[guild_id]
+        )
+        await message.edit(embed=output_formatter.server_synced(embed, description = f'Synced to {guild_id}'))
+    except Exception as e:
+        await message.edit(embed=output_formatter.error(embed, e, traceback.format_exc()))
+
 @bot.slash_command(description="Loads an extension")
 async def load(interaction, extension):
 
     await interaction.response.defer()
-    embed = output.embed(title="Load Extension", description = f'Loading {extension}')
+    embed = output_formatter.embed(title="Load Extension", description = f'Loading {extension}')
     message = await interaction.followup.send(embed=embed)
 
     try:
         bot.load_extension(f'app.cogs.{extension}')
-        await message.edit(embed=output.update(embed, description = f'Loaded {extension}'))
+        await message.edit(embed=output_formatter.update(embed, description = f'Loaded {extension}'))
     except Exception as e:
-        await message.edit(embed=output.error(embed, e, traceback.format_exc()))
+        await message.edit(embed=output_formatter.error(embed, e, traceback.format_exc()))
 
 @bot.slash_command(description="Reloads an extension")
 async def reload(interaction, extension):
 
     await interaction.response.defer()
-    embed = output.embed(title="Reload Extension", description = f'Reloading {extension}')
+    embed = output_formatter.embed(title="Reload Extension", description = f'Reloading {extension}')
     message = await interaction.followup.send(embed=embed)
 
     try:
         bot.reload_extension(f'app.cogs.{extension}')
-        await message.edit(embed=output.update(embed, description = f'Reloaded {extension}'))
+        await message.edit(embed=output_formatter.update(embed, description = f'Reloaded {extension}'))
     except Exception as e:
-        await message.edit(embed=output.error(embed, e, traceback.format_exc()))
+        await message.edit(embed=output_formatter.error(embed, e, traceback.format_exc()))
 
 @bot.slash_command(description="Unloads an extension")
 async def unload(interaction, extension):
 
     await interaction.response.defer()
-    embed = output.embed(title="Unload Extension", description = f'Unloading {extension}')
+    embed = output_formatter.embed(title="Unload Extension", description = f'Unloading {extension}')
     message = await interaction.followup.send(embed=embed)
     
     try:
         bot.unload_extension(f'app.cogs.{extension}')
-        await message.edit(embed=output.update(embed, description = f'Unloaded {extension}'))
+        await message.edit(embed=output_formatter.update(embed, description = f'Unloaded {extension}'))
     except Exception as e:
-        await message.edit(embed=output.error(embed, e, traceback.format_exc()))
+        await message.edit(embed=output_formatter.error(embed, e, traceback.format_exc()))
     
 
 @tasks.loop(count=1)
